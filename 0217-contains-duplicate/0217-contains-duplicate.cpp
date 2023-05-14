d9b3c11c2416c00b2d7f7a58e9406af471d2a11b
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2023-05-14 12:25:04 +0530

    Time: 168 ms (30.49%), Space: 71.6 MB (21.21%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums)
+    {
+        unordered_map<int,int> mp;
+        int i = 0 , n = nums.size();
+        for(i = 0 ; i < n ; i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(i = 0 ; i < n ; i++)
+        {
+            if(mp[nums[i]] > 1)
+                return true;
+        }
+        return false;
+        
+    }
+};
