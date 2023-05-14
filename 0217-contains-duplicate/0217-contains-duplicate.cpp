7e3f60fb57ef42a8edd5c123ed5e57bfb2a9de34
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2023-05-14 12:26:46 +0530

    Time: 149 ms (47.59%), Space: 57.2 MB (94.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums)
+    bool containsDuplicate(vector<int>& nums) 
     {
-        unordered_map<int,int> mp;
-        int i = 0 , n = nums.size();
-        for(i = 0 ; i < n ; i++)
+        sort(nums.begin(), nums.end());
+        for(int i = 0 ; i < nums.size() - 1; i++)
         {
-            mp[nums[i]]++;
-        }
-        for(i = 0 ; i < n ; i++)
-        {
-            if(mp[nums[i]] > 1)
+            if(nums[i] == nums[i+1])
                 return true;
         }
         return false;
