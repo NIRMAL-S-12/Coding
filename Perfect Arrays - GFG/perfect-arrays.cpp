93534deb4a3a9734d7d64587d167460584170a3b
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2023-05-29 19:31:08 +0530

    Added solution - LeetHub

---
+++ Perfect Arrays - GFG/perfect-arrays.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool IsPerfect(int a[],int n)
+    {
+        // Complete the function
+        int j = n - 1;
+        for(int i = 0 ; i < n / 2 ; i++)
+        {
+            if(a[i] != a[j])
+            return false;
+            j--;
+        }
+        return true;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        if(ob.IsPerfect(a,n))
+            cout<<"PERFECT\n";
+        else
+            cout<<"NOT PERFECT\n";
+        
+    }
+    
+}
+// } Driver Code Ends
