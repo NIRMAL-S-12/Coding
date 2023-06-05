54ec5362569861d4138b5daa45289314eaafb80e
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2023-06-05 18:24:04 +0530

    Added solution - LeetHub

---
+++ Sum of AP series - GFG/sum-of-ap-series.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+	public:
+		long sum_of_ap(long n,long a, long d)
+		{
+		    // Code here.
+		    return n * ((n - 1) * d + a + a) / 2;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long n, a, d;
+    	cin >> n >> a >> d;
+    	Solution ob;
+    	long ans = ob.sum_of_ap(n, a, d);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
