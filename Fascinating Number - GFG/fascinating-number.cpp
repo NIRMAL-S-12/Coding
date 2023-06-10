c67713669f60262d15e3dadc40a08a1e88f08862
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2023-06-10 21:01:49 +0530

    Added solution - LeetHub

---
+++ Fascinating Number - GFG/fascinating-number.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	bool fascinating(int n) 
+	{
+	    // code here
+	    string a = to_string(n) + to_string(n * 2) + to_string(n * 3);
+	    sort(a.begin(), a.end());
+	    //cout<<a;
+	    return a == "123456789";
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.fascinating(n);
+        if (ans) {
+            cout << "Fascinating\n";
+        } else {
+            cout << "Not Fascinating\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
