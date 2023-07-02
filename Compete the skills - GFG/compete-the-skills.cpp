ac3c3f9b846e2c427820c128b8456c716e3e526c
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2023-07-02 08:46:56 +0530

    Added solution - LeetHub

---
+++ Compete the skills - GFG/compete-the-skills.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void scores(long long a[], long long b[], int &ca, int &cb)
+    {
+        // Your code goes here   
+        int ans1 = 0 , ans2 = 0;
+        for(int i = 0 ; i < 3 ; i++)
+        {
+            if(a[i] > b[i])
+            ans1++;
+            else if(a[i] < b[i])
+            ans2++;
+        }
+        ca = ans1;
+        cb = ans2;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t=0;
+    cin>>t;
+    while(t--)
+    {
+        long long int a[5], b[5], i=0;
+        int ca=0, cb=0;
+        for(i=0; i<3; i++)
+            cin>>a[i];
+    
+        for(i=0; i<3; i++)
+            cin>>b[i];
+        Solution ob;
+        ob.scores(a, b, ca, cb);
+        
+        cout<<ca<<" "<<cb<<endl;
+    }
+    return 0 ;
+}
+// } Driver Code Ends
