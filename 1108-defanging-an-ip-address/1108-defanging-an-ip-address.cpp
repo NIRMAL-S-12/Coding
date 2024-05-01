8dbeeb4bc65e88862b6b0fa7769a3764ec73e7fb
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-01 15:44:18 +0530

    Time: 2 ms (51.92%), Space: 7.2 MB (18.33%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) 
+    {
+        string res = "";
+        for(char ch : address)
+        {
+            if(ch == '.')
+                res += "[.]";
+            else
+                res += ch;
+        }
+        return res;
+        
+    }
+};
