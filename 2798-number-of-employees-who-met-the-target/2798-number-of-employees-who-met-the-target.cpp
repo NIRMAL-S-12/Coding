6144370553e6b96793b212a15c2cefa4ec10737d
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-02 10:46:05 +0530

    Time: 3 ms (80.53%), Space: 23.3 MB (15.11%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) 
+    {
+        int ans = 0;
+        for(int i : hours)
+        {
+            if(i >= target) ans++;
+        }
+        return ans;
+        
+    }
+};
