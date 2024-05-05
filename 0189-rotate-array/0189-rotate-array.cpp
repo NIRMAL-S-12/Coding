9cbbcd59f3077749fbf67e3c00eb3b977a04eec0
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-05 18:08:57 +0530

    Time: 15 ms (92.51%), Space: 27.3 MB (81.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) 
     {
          k = k % nums.size();
-        reverse(nums.begin() + nums.size() - k , nums.end());
+        reverse(nums.end() - k , nums.end());
         reverse(nums.begin(), nums.end() - k);
         reverse(nums.begin(), nums.end());
         
