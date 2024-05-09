7360fdc90b628cd5b12c8e59e104a5f38f2859cb
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-09 11:23:14 +0530

    Time: 3 ms (92.34%), Space: 9.4 MB (81.03%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    int fun(char ch)
+    {
+        if(ch == 'I')
+            return 1;
+        if(ch == 'V')
+            return 5;
+        if(ch == 'X')
+            return 10;
+        if(ch == 'L')
+            return 50;
+        if(ch == 'C')
+            return 100;
+        if(ch == 'D')
+            return 500;
+        if(ch == 'M')
+            return 1000;
+        return -1;
+    }
+    int romanToInt(string s) 
+    {
+        int ans = 0;
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if((i + 1) < s.size() && fun(s[i+1]) > fun(s[i]))
+            {
+                ans += (fun(s[i+1]) - fun(s[i]));
+                i+=1;
+            }
+            else
+            {
+                ans += fun(s[i]);
+            }
+        }
+        return ans;
+        
+    }
+};
