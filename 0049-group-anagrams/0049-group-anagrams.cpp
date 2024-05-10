8ed085c5822f5033678f2c33019ed054fc9232aa
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-10 11:10:05 +0530

    Time: 33 ms (32.28%), Space: 23.7 MB (74.21%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        vector<vector<string>> ans;
+        map<string, vector<string>> mp;
+        for(int i = 0 ; i < strs.size() ; i++)
+        {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            auto it = mp.find(s);
+            if(it != mp.end())
+            {
+                mp[s].push_back(strs[i]);
+            }
+            else
+            {
+                mp[s].push_back(strs[i]);
+            }
+        }
+        for(auto &i : mp)
+        {
+            ans.push_back(i.second);
+        }
+        return ans;
+        
+    }
+};
