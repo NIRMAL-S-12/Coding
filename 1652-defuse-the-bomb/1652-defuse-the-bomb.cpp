89f9ce2b4789868088bb08106d82ec851ecd4c9c
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-11 11:20:18 +0530

    Time: 6 ms (10.98%), Space: 9.9 MB (95.02%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) 
+    {
+        vector<int> ans(code.size(), 0);
+        if(k == 0)
+        {
+            for(int i = 0 ; i < code.size() ; i++)
+                code[i] = 0;
+            return code;
+        }
+        else if(k > 0)
+        {
+            for(int i = 0 ; i < code.size(); i++)
+            {
+                for(int j = 1 ; j <= k ; j++)
+                {
+                    ans[i] += code[(i+j) % code.size()];
+                }
+            }
+            return ans;
+        }
+        else
+        {
+            for(int i = 0 ; i < code.size() ; i++)
+            {
+                for(int j = 1 ; j <= -k ; j++)
+                {
+                    ans[i] += code[(i-j+code.size()) % code.size()];
+                }
+            }
+            return ans;
+        }
+        return {0};
+    }
+};
