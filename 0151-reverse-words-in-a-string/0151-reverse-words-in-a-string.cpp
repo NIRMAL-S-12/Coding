f4ac467f9eaf1c508393544e214075960677174b
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-05-12 21:08:46 +0530

    Time: 7 ms (36.86%), Space: 70.1 MB (5.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     string reverseWords(string s) 
     {
-        if (s.size() == 0)
-        return "";
-
-    int i = 0;
-    while (i < s.size() && s[i] == ' ')
-        i++;
-
-    if (i == s.size())
-        return "";
-
-    int j = i;
-    while (j < s.size() && s[j] != ' ')
-        j++;
-
-    std::string word = s.substr(i, j - i);
-    std::string res = reverseWords(s.substr(j));
-
-    if (res == "")
-        return word;
-    
+        if(s.size() == 0)
+            return "";
+        int i = 0;
+        while(i < s.size() && s[i] == ' ')
+            i++;
+        if(i == s.size())
+            return "";
+        int j = i;
+        while(j < s.size() && s[j] != ' ')
+            j++;
+        
+        string word = s.substr(i , j - i);
+        
+        string res = reverseWords(s.substr(j));
+        
+        if(res == "")
+            return word;
         return res + " " + word;
+        
     }
 };
