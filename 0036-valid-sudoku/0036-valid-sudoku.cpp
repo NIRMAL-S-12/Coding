ca8052b00d540bd467106853c4353513a49026d8
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-06-26 10:11:43 +0530

    Time: 20 ms (37.62%), Space: 24.9 MB (22.03%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        unordered_map<char, int> row[9], col[9],mat[9];
+        map<char, int> row[9], col[9], sub[9];
         for(int i = 0 ; i < board.size() ; i++)
         {
             for(int j = 0 ; j < board[i].size() ; j++)
             {
                 char ch = board[i][j];
-                if(ch != '.' && (row[i][ch]++ > 0 || col[j][ch]++ > 0 || mat[i/3 * 3 + j / 3][ch]++ > 0))
+                if(ch != '.' && (row[i][ch]++ > 0 || col[j][ch]++ > 0 || sub[i/3*3 + j/3][ch]++ > 0))
                     return false;
             }
+            
         }
         return true;
+        
     }
 };
