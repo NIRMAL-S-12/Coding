ef50bbf2539bc49a6546508bc3662a49764a9873
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-06-26 09:50:16 +0530

    Time: 30 ms (45.73%), Space: 24.9 MB (50.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,8 +2,30 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) 
     {
-        vector<vector<string>> ans;
-        map<string, vector<string>> mp;
+//         vector<vector<string>> ans;
+//         map<string, vector<string>> mp;
+//         for(int i = 0 ; i < strs.size() ; i++)
+//         {
+//             string s = strs[i];
+//             sort(s.begin(), s.end());
+//             auto it = mp.find(s);
+//             if(it != mp.end())
+//             {
+//                 mp[s].push_back(strs[i]);
+//             }
+//             else
+//             {
+//                 mp[s].push_back(strs[i]);
+//             }
+//         }
+//         for(auto &i : mp)
+//         {
+//             ans.push_back(i.second);
+//         }
+//         return ans;
+        
+        
+        map<string, vector<string>>mp;
         for(int i = 0 ; i < strs.size() ; i++)
         {
             string s = strs[i];
@@ -18,7 +40,8 @@ public:
                 mp[s].push_back(strs[i]);
             }
         }
-        for(auto &i : mp)
+        vector<vector<string>>ans;
+        for(auto i : mp)
         {
             ans.push_back(i.second);
         }
