147617dd5343d628f077b4179523f605c3a029ff
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-06-26 08:11:52 +0530

    Time: 4 ms (73.78%), Space: 9.8 MB (84.47%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string intToRoman(int num) 
+    {
+        string ones[] = {"", "I" , "II" , "III" , "IV" , "V" , "VI" , "VII" , "VIII" , "IX" };
+        string tens[] = {"" , "X" , "XX" , "XXX" , "XL" , "L" , "LX" , "LXX" , "LXXX" , "XC"};
+        string hrds[] = {"" , "C" , "CC" , "CCC" , "CD" , "D" , "DC" , "DCC" , "DCCC" , "CM"};
+        string thos[] = {"" , "M" , "MM" , "MMM"};
+        
+        return thos[num/1000] + hrds[(num%1000) / 100] + tens[(num%100) / 10] + ones[(num%10)];
+        
+    }
+};
