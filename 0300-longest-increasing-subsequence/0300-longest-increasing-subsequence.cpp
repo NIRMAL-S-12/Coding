a530aee41a3b9c6105accf4efabe557c82305a56
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-06-26 21:04:50 +0530

    Time: 175 ms (39.06%), Space: 42 MB (34.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,24 +2,33 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) 
     {
-        if (nums.empty()) return 0;
-    
-    vector<int> dp(nums.size(), 1); // Initialize DP array with 1, as minimum length of LIS is 1
-    
-    int maxLength = 1; // Minimum length of LIS is 1
-
-    for (int i = 1; i < nums.size(); ++i) 
-    {
-        for (int j = 0; j < i; ++j) 
+        set<int>s(nums.begin(), nums.end());
+        vector<int>v(s.begin(), s.end());
+        int m = v.size();
+        int n = nums.size();
+        int dp[n+1][m+1];
+        for(int i = 0 ; i <= n ; i++)
         {
-            if (nums[i] > nums[j]) 
+            for(int j = 0 ; j <= m ; j++)
             {
-                dp[i] = max(dp[i], dp[j] + 1);
+                dp[i][j] = 0;
             }
         }
-        maxLength = max(maxLength, dp[i]);
-    }
-
-    return maxLength;   
+        
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= m ; j++)
+            {
+                if(nums[i - 1] == v[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
