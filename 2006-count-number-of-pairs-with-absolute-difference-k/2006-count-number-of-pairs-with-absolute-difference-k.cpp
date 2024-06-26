0c377423686345d69d57817ff70d9f4ec808aac4
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-06-26 07:56:30 +0530

    Time: 4 ms (87.54%), Space: 15 MB (39.89%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) 
+    {
+        int mp[201];
+	    for(int i = 0 ; i < nums.size() ; i++)
+	    mp[nums[i]]++;
+	    
+	    int ans = 0;
+	    for(int i = k + 1; i < 201 ; i++)
+	    {
+	        ans += mp[i] * mp[i - k];
+	    }
+	    return ans ;
+        
+    }
+};
