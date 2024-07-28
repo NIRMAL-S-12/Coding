8601082c23ba55f14369e99d7c500e574f1dda8d
Nim <90773890+NIRMAL-S-12@users.noreply.github.com>
2024-07-28 11:31:27 +0530

    Time: 2 ms (17.73%), Space: 42.5 MB (15.48%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,17 +1,18 @@
-class Solution {
-    
-    public int hcf(int a, int b)
+class Solution 
+{
+    int gcd(int a, int b)
     {
         if(b == 0)
             return a;
-        return hcf(b, a%b);
+        return gcd(b , a%b);
     }
     public String gcdOfStrings(String str1, String str2) 
     {
-        int n = str1.length(), m = str2.length();
-        int gcd = hcf(n,m);
+        int len1 = str1.length(), len2 = str2.length();
+        int hcf = gcd(len1, len2);
+        System.out.println(hcf);
         if((str1 + str2).equals(str2 + str1))
-        return str1.substring(0,gcd);
+        return str1.substring(0,hcf);
         return "";
     }
 }
